41e9c9d00bb3133d5b6bd9c20b1bc445be4a86bc
Python-Lettle <1071445082@qq.com>
2019-07-22 14:41:15 +0800

    Rewrite DUI

--- bin/widget.py
+++ bin/widget.py
@@ -1,17 +1,12 @@
-#-*-coding:utf8;-*-
-#qpy:3
-#qpy:console
-#python3
+#-*- coding:utf-8 -*-
+
+#DUI库的基础控件
 __author__='Lettle'
-import os,sys,re
-from ctypes import *
-#基础方法
-def slen(value):
-    length =len(value)
-    utf8_length =len(value.encode('utf-8'))
-    length =(utf8_length -length)/2+length
-    return int(length)
-#控件库-----------------------------------------------------------
+
+from bin.functions import *
+import os,sys
+
+#Widgets
 class Window():
     def __init__(self,title,mark):
         self.mark = mark
@@ -19,21 +14,21 @@ class Window():
         self.sys = "Linux"
         self.height = 20
         self.width = 40
-        self.widget = []
+        self.widget = {}
     def set_sys(self,sys):
         self.sys = sys
     def set_size(self,height,width):
         self.height = int(height)
         self.width = int(width)
-    def add_widget(self,widget_name,widget_location,way='L',text='widget',cursor_index=0,onclick_sen="pass"):
+    def add_widget(self,widget_name,widget_location,way='L',text='widget',cursor_index=0,onclick_sen="pass",wi_id=None):
         for i in range(1):        #更新控件库记得修改这里---------
             if widget_name == "line" or widget_name == 'Line':
-                widget = Line(widget_location-1)
+                widget = Line(widget_location-1,w_id=wi_id)
             elif widget_name == 'TextLine':
-                widget = TextLine(text,way,widget_location-1)
+                widget = TextLine(text,way,widget_location-1,w_id=wi_id)
             elif widget_name == 'Button':
-                widget = Button(text,way,widget_location-1,cursor_index=cursor_index,onclick_sent=onclick_sen)
-        self.widget.append(widget)
+                widget = Button(text,way,widget_location-1,cursor_index=cursor_index,onclick_sent=onclick_sen,w_id=wi_id)
+        self.widget[wi_id]=widget
     def buildW(self,Frame):
         if self.sys == "Windows":  #判断系统进行清屏
             os.system("cls")
@@ -58,12 +53,14 @@ class Window():
                         widget_task[i].select=False
 
         widget_a = len(self.widget)
+        widget_task = []
         if widget_a == 0:        #判断有无控件
             for i in range(self.height):
                 if build_fbte(i):
                     print("║"+" "*(self.width-2)+"║")
         else:
-            widget_task = self.widget
+            for key,value in self.widget.items():
+                widget_task.append(value)
             a = 0 #控制下面空白区域的显示
             Button_num = 0
             refresh_button()
@@ -94,7 +91,7 @@ class Window():
                                     if widget_task[ii].select == True:
                                         color = '32'
                                     elif widget_task[ii].select == False:
-                                        color = '30'
+                                        color = '37'
                                     if slen(widget_task[ii].text)>(self.width-2):
                                         text = re.findall(r'.{'+str(self.width-2)+r'}',widget_task[ii].text)
                                         print(color+"[1;31;40m%s"%text+color+"[0m")  
@@ -122,23 +119,23 @@ class Window():
                         print("║"+" "*(self.width-2)+"║")
 
 class Line():
-    def __init__(self,location):
+    def __init__(self,location,w_id=None):
         self.mark = 0
         self.location = location
 class TextLine():
-    def __init__(self,text,way,location):
+    def __init__(self,text,way,location,w_id=None):
         self.mark = 1
         self.text = text
         self.way = way
         self.location = location
 class Form():
-    def __init__(self,th,data,way,location):
+    def __init__(self,th,data,way,location,w_id=None):
         self.mark = 2
         self.th = th
         self.way = way
         self.location = location
 class Button():
-    def __init__(self,text,way,location,cursor_index,onclick_sent="pass"):
+    def __init__(self,text,way,location,cursor_index,onclick_sent="pass",w_id=None):
         self.mark = 3
         self.select = False
         self.location = location
@@ -153,11 +150,8 @@ class Listener():
         self.mark = 4
         self.running = False
     def run(self,Fram,mark=0,confirm_key="y",kill_key="q"):
-        c = cdll.LoadLibrary(os.getcwd()+"/getchar.so")
         while self.running:
-            key = chr(c.get_char())
-            #sys.stdout.write('%c'%string)
-            #sys.stdout.flush()
+            key = Getch()
             if key == "w":
                 if Fram.cursor > 0 and Fram.Button_num != 1:
                     Fram.cursor -= 1
@@ -169,39 +163,10 @@ class Listener():
                 break
             Fram.build(0)
             if key == confirm_key: #确认键
-                window_widgets = Fram.windows[mark].widget
+                window_widgets = []
+                for keys,value in Fram.windows[mark].widget.items():
+                    window_widgets.append(value)
                 for i in range(len(window_widgets)):
                     if window_widgets[i].mark == 3:
                         if window_widgets[i].select:
                             window_widgets[i].onclick()
-#主框架-----------------------------------------------------------
-class Frame():
-    def __init__(self):
-        self.windows = []
-        self.Listener = Listener(False)
-        self.cursor = 0
-        self.Button_num = 0
-    def add_window(self,wd):
-        self.windows.append(wd)
-    def listen(self,mark_=0,confirm="y",kill="q"):
-        self.Listener.running = True
-        self.Listener.run(self,mark=mark_,confirm_key=comfirm,kill_key=kill)
-    def build(self,mark=0):
-        self.windows[mark].buildW(self)
-
-#用例-----------------------------------------------
-if __name__=="__main__":
-    #用例-----------------------------------------------
-if __name__=="__main__":
-    t = Frame()
-    main_w = Window('主窗口',0)
-    main_w.add_widget('TextLine',3,way='C',text='DUI测试界面')
-    main_w.add_widget('line',4)
-    main_w.add_widget('TextLine',5,text='你可以用w向上s向下,y键确认,q键退出')
-    main_w.add_widget('Button',6,text='测试按钮1',cursor_index=0,onclick_sen=r"print('1号成功')")
-    main_w.add_widget('Button',7,text='测试按钮2',cursor_index=1,onclick_sen=r"print('2号成功')")
-    main_w.add_widget('Button',8,text='测试按钮3',cursor_index=2,onclick_sen=r"print('3号成功')")
-    t.add_window(main_w)
-    t.build(0)
-    t.listen(0,confirm="y",kill="q")
-
--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-import setuptools
-
-with open('README.rst', 'r', encoding='utf8') as fh:
-    long_description = fh.read()
-
-setuptools.setup(name='xuzhao-markdown-editor',
-                 version='1.0',
-                 description='xuzhao markdown editor',
-                 long_description=long_description,
-                 long_description_content_type="text/markdown",
-                 author='xuzhao',
-                 author_email='markdown@felinae.net',
-                 url='https://markdown.felinae.net',
-                 keywords='django markdown editor editormd',
-                 packages=setuptools.find_packages(),
-                 zip_safe=False,
-                 include_package_data=True,
-                 classifiers=(
-                     "Programming Language :: Python",
-                     "Development Status :: 4 - Beta",
-                     "Operating System :: OS Independent",
-                     "License :: OSI Approved :: Apache Software License",
-                     "Framework :: Django"
-                 ))
+'''
+暂时不支持setup.py的安装方式
+（其实是作者太菜了）
+'''
