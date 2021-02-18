e6f5d266c1e4874632d874b35c35a29e4651a5e0
Python-Lettle <1071445082@qq.com>
2021-02-18 20:30:03 +0800

    Fixed a bug

--- guess.py
+++ guess.py
@@ -33,7 +33,7 @@ def guess():
 			continue
 		if res == an:
 			# 猜对
-			gameWindow.updateWidget(4, Text())
+			gameWindow.updateWidget(0, Alert("猜错了！不是"+str(res)))
 			frame.addWindow(gameWindow, 1)
 			frame.showWindow(2)
 			frame.listener.run()
